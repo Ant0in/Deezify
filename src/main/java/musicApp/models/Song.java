c2157cff3a6b76e556f69d34e443e132fe7a0752
gabgolds <gabriel.goldsztajn@ulb.be>
2025-03-30 18:17:08 +0200

    Refactor Song model to use composition with Metadata model

--- src/main/java/musicApp/models/Song.java
+++ src/main/java/musicApp/models/Song.java
@@ -3,8 +3,6 @@ package musicApp.models;
 import com.google.gson.annotations.Expose;
 import javafx.scene.image.Image;
 import javafx.util.Duration;
-import musicApp.exceptions.BadFileTypeException;
-import musicApp.exceptions.ID3TagException;
 import musicApp.utils.MetadataUtils;
 import org.jaudiotagger.tag.images.Artwork;
 
@@ -17,12 +15,7 @@ public class Song {
 
     @Expose
     private final Path filePath;
-    private String title;
-    private String artist;
-    private String genre;
-    private byte[] cover;
-    private ArrayList<String> userTags;
-    private Duration duration;
+    private Metadata metadata;
 
     /**
      * Constructor from MetadataReader.
@@ -33,194 +26,96 @@ public class Song {
         this.filePath = filePath;
         MetadataUtils metadataReader = new MetadataUtils();
         try {
-            Metadata metadata = metadataReader.getMetadata(filePath.toFile());
-            this.setMetadata(metadata);
-        } catch (ID3TagException | BadFileTypeException e) {
-            Metadata defaultMetadata = new Metadata();
-            this.setMetadata(defaultMetadata);
+            this.metadata = metadataReader.getMetadata(filePath.toFile());
+        } catch (Exception e) {
+            this.metadata = new Metadata(); // Default metadata on error
         }
-    }
-
-    public void reloadMetadata() {
-
-        // ?? might wanna throw here idk
-
-        MetadataUtils metadataReader = new MetadataUtils();
-
-        try {
-            Metadata metadata = metadataReader.getMetadata(filePath.toFile());
-            this.setMetadata(metadata);
-        } catch (ID3TagException | BadFileTypeException e) {
-            Metadata defaultMetadata = new Metadata();
-            this.setMetadata(defaultMetadata);
-        }
-
     }
 
     /**
-     * Get song file path
-     *
-     * @return The Path to the song file.
+     * Reload metadata for the song.
      */
+    public void reloadMetadata() {
+        MetadataUtils metadataReader = new MetadataUtils();
+        try {
+            this.metadata = metadataReader.getMetadata(filePath.toFile());
+        } catch (Exception e) {
+            this.metadata = new Metadata(); // Default metadata on error
+        }
+    }
+
     public Path getFilePath() {
         return filePath;
     }
 
-    /**
-     * Get the name of the song.
-     *
-     * @return The name of the song.
-     */
     public String getTitle() {
-        return title;
+        return metadata.getTitle();
     }
 
-    /**
-     * Set the name of the song.
-     *
-     * @param title The name of the song.
-     */
     public void setTitle(String title) {
-        this.title = title;
+        metadata.setTitle(title);
     }
 
-    /**
-     * Get the name of the artist.
-     *
-     * @return The name of the artist.
-     */
     public String getArtist() {
-        return artist;
+        return metadata.getArtist();
     }
 
-    /**
-     * Set the name of the artist.
-     *
-     * @param artist The name of the artist.
-     */
     public void setArtist(String artist) {
-        this.artist = artist;
+        metadata.setArtist(artist);
     }
 
-    /**
-     * Get the genre of the song.
-     *
-     * @return The genre of the song.
-     */
     public String getGenre() {
-        return genre;
+        return metadata.getGenre();
     }
 
-    /**
-     * Set the genre of the song.
-     *
-     * @param genre The genre of the song.
-     */
     public void setGenre(String genre) {
-        this.genre = genre;
+        metadata.setGenre(genre);
     }
 
-    /**
-     * Get the path to the cover image.
-     *
-     * @return The path to the cover image.
-     */
+    public Duration getDuration() {
+        return metadata.getDuration();
+    }
+
+    public void setDuration(Duration duration) {
+        metadata.setDuration(duration);
+    }
+
     public byte[] getCover() {
-        return cover;
+        return metadata.getCover() != null ? metadata.getCover().getBinaryData() : null;
     }
 
-    /**
-     * Set the path to the cover image.
-     *
-     * @param cover the cover in base64.
-     */
-    public void setCover(Artwork cover) {
-        this.cover = cover.getBinaryData();
+    public void setCover(Artwork artwork) {
+        metadata.setCover(artwork);
     }
 
-    /**
-     * Get the cover as a JavaFX Image.
-     *
-     * @return The cover image.
-     */
+    public ArrayList<String> getUserTags() {
+        return metadata.getUserTags();
+    }
+
+    public void setUserTags(ArrayList<String> userTags) {
+        metadata.setUserTags(userTags);
+    }
+
     public Image getCoverImage() {
-        if (this.cover == null) {
+        if (getCover() == null) {
             return new Image(Objects.requireNonNull(getClass().getResource("/images/song.png")).toExternalForm());
         }
         try {
-            return new Image(new ByteArrayInputStream(cover));
+            return new Image(new ByteArrayInputStream(getCover()));
         } catch (Exception e) {
             return new Image(Objects.requireNonNull(getClass().getResource("/images/song.png")).toExternalForm());
         }
     }
 
-    public ArrayList<String> getUserTags() {
-        return userTags;
-    }
-
-    public void setUserTags(ArrayList<String> userTags) {
-        this.userTags = userTags;
-    }
-
-    /**
-     * Get the duration of the song.
-     *
-     * @return The duration of the song.
-     */
-    public Duration getDuration() {
-        return duration;
-    }
-
-    /**
-     * Set the duration of the song.
-     *
-     * @param duration The duration of the song.
-     */
-    public void setDuration(Duration duration) {
-        this.duration = duration;
-    }
-
     public Metadata getMetadata() {
-        Metadata metadata = new Metadata();
-        metadata.setTitle(title);
-        metadata.setArtist(artist);
-        metadata.setGenre(genre);
-        metadata.setCoverFromBytes(cover);
-        metadata.setUserTags(userTags);
-        metadata.setDuration(duration);
         return metadata;
     }
 
-    /**
-     * Loads Metadata from MetadataReader.
-     *
-     * @param metadata The metadata of the song.
-     */
-    private void setMetadata(Metadata metadata) {
-        this.title = metadata.getTitle();
-        this.artist = metadata.getArtist();
-        this.genre = metadata.getGenre();
-        this.cover = metadata.getCover() != null ? metadata.getCover().getBinaryData() : null;
-        this.userTags = metadata.getUserTags();
-        this.duration = metadata.getDuration();
-    }
-
-    /**
-     * Get the string representation of the song.
-     *
-     * @return The string representation of the song.
-     */
     @Override
     public String toString() {
-        return title + " - " + artist;
+        return getTitle() + " - " + getArtist();
     }
 
-    /**
-     * Check if two songs are equal by comparing their file paths.
-     *
-     * @param obj The object to compare.
-     * @return True if the songs are equal, false otherwise.
-     */
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Song song) {
@@ -229,18 +124,10 @@ public class Song {
         return false;
     }
 
-    /**
-     * Check if the song contains the given text.
-     *
-     * @param text The text to search for.
-     * @return True if the song contains the text, false otherwise.
-     */
     public Boolean containsText(String text) {
         String lowerText = text.toLowerCase();
-        return this.title.toLowerCase().contains(lowerText)
-                || this.artist.toLowerCase().contains(lowerText)
-                || this.genre.toLowerCase().contains(lowerText);
+        return getTitle().toLowerCase().contains(lowerText)
+                || getArtist().toLowerCase().contains(lowerText)
+                || getGenre().toLowerCase().contains(lowerText);
     }
-
 }
-
